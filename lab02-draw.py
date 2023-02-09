6f290dcfea53cabfd573454c158122751b13ec88
paulbate <123836226+chillingq@users.noreply.github.com>
2023-02-09 20:17:49 +0100

    Add files via upload

--- lab02-draw.py
+++ lab02-draw.py
@@ -1,3 +1,22 @@
+<<<<<<< HEAD
+import arcade
+
+
+
+
+# Import arcade module
+
+"""
+ESTRELLA DE DAVID
+"""
+arcade.open_window(599, 599, "Drawing Example")
+arcade.set_background_color(arcade.csscolor.CRIMSON)
+
+arcade.start_render()
+arcade.draw_triangle_filled(129, 179, 299, 549, 469, 179, arcade.csscolor.BLACK)
+arcade.draw_triangle_filled(129, 419, 299, 49, 469, 419, arcade.csscolor.BLACK)
+arcade.finish_render()
+=======
 import arcade
 
 # Import arcade module
@@ -12,4 +31,5 @@ arcade.start_render()
 arcade.draw_triangle_filled(100, 150, 300, 550, 500, 150, arcade.csscolor.BLACK)
 arcade.draw_triangle_filled(100, 449, 300, 50, 500, 449, arcade.csscolor.BLACK)
 arcade.finish_render()
+>>>>>>> eec112e4ab64526c4a80b8d10dbe0e736a39cb94
 arcade.run()
