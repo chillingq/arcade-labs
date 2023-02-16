2bb36f2d5a06c549566351546346275373db0996
paulbate <miguel.romanp@edu.uah.es>
2023-02-16 19:11:39 +0100

    Update lab02-draw.py

--- lab02-draw.py
+++ lab02-draw.py
@@ -1,3 +1,32 @@
+<<<<<<< HEAD
+import arcade
+
+=======
+<<<<<<< HEAD
+import arcade
+
+
+
+
+>>>>>>> d31640cbe5daa00944275327bcb14737e94f0b0c
+# Import arcade module
+
+"""
+ESTRELLA DE DAVID
+"""
+arcade.open_window(599, 599, "Drawing Example")
+arcade.set_background_color(arcade.csscolor.CRIMSON)
+
+arcade.start_render()
+<<<<<<< HEAD
+arcade.draw_triangle_filled(100, 150, 300, 550, 500, 150, arcade.csscolor.BLACK)
+arcade.draw_triangle_filled(100, 449, 300, 50, 500, 449, arcade.csscolor.BLACK)
+arcade.finish_render()
+=======
+arcade.draw_triangle_filled(129, 179, 299, 549, 469, 179, arcade.csscolor.BLACK)
+arcade.draw_triangle_filled(129, 419, 299, 49, 469, 419, arcade.csscolor.BLACK)
+arcade.finish_render()
+=======
 import arcade
 
 # Import arcade module
@@ -12,4 +41,6 @@ arcade.start_render()
 arcade.draw_triangle_filled(100, 150, 300, 550, 500, 150, arcade.csscolor.BLACK)
 arcade.draw_triangle_filled(100, 449, 300, 50, 500, 449, arcade.csscolor.BLACK)
 arcade.finish_render()
+>>>>>>> eec112e4ab64526c4a80b8d10dbe0e736a39cb94
+>>>>>>> d31640cbe5daa00944275327bcb14737e94f0b0c
 arcade.run()
